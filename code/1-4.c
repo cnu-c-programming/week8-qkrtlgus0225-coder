926b5c0b6b76bf8222c97490b78d18a4af80077d
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-04-24 11:52:32 +0900

    Update 1-4.c

---
+++ code/1-4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int my_strcmp(const char *a, const char *b) {
+    while (*a != '\0' && (*a == *b)) {
+        a++;
+        b++;
+    }
+
+  int main(){
+    printf("%d\n", my_strcmp("abc", "abcd"));
+     printf("%d\n", my_strcmp("abc", "abc"));
+     printf("%d\n", my_strcmp("b", "a"));
+  
+    return 0;
+}
