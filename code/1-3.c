59d4f56c45bbb77e6312bf8c7c9c5667662282f3
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-04-24 12:02:47 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -15,15 +15,16 @@ char *my_strcat(char *dst, const char *src) {
     
     *ptr = '\0';
     
+    return dst;
 }
 
-int main(){
-  char str1[20] = "Hello";
-  char str2[] = "World"
-
-  printf("%s\n", str1);
-  my_strcat(str1,str2);
+int main() {
+    char str1[20] = "Hello";
+    char str2[] = "World";
+    
     printf("%s\n", str1);
-
-  return 0;
+    my_strcat(str1, str2);
+    printf("%s\n", str1);
+    
+    return 0;
 }
