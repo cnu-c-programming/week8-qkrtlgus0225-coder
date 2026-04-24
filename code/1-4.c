c5dcaf7778e97751ff3778dccef0b54b02071569
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-04-24 12:03:05 +0900

    Update 1-4.c

--- code/1-4.c
+++ code/1-4.c
@@ -5,11 +5,14 @@ int my_strcmp(const char *a, const char *b) {
         a++;
         b++;
     }
+    
+    return *(unsigned char *)a - *(unsigned char *)b;
+}
 
-  int main(){
+int main() {
     printf("%d\n", my_strcmp("abc", "abcd"));
-     printf("%d\n", my_strcmp("abc", "abc"));
-     printf("%d\n", my_strcmp("b", "a"));
-  
+    printf("%d\n", my_strcmp("abc", "abc" ));
+    printf("%d\n", my_strcmp("b"  , "a"   ));
+    
     return 0;
 }
